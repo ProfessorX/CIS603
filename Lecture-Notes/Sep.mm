34c3f5dfb1535d36fcfdc8299e930b86cbbfdeba
Abraham Xiao <q289196573@gmail.com>
2014-09-17 21:13:54 +0400

    Course notes updated. Dr. Talah mentioned that I should prove to Dr. Sid that he has made the wrong decision. All in all, it is me to blame. I put myself in a situation like this, but I am a genius. I am. Or I would have committed suicide a long time ago.

---
+++ Lecture-Notes/Sep.mm
@@ -0,0 +1,90 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node text="Sep.org" background_color="#00bfff">
+<richcontent TYPE="NOTE"><html><head></head><body><p>--org-mode: WHOLE FILE</p></body></html></richcontent>
+<node text="September, 2014" position="left">
+<node text="20140831">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>- We model the world by &quot;Games&quot;.<br />- Game theory and repeated interactions.<br />- Decision making.<br />- Social choices and mechanisms.<br />- [ ] Just as usual, the project matters.</p><p></p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="20140903">
+<node text="Nash bargaining solution" background_color="#ffff88">
+<icon builtin="button_cancel"/>
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>- Repetition</p><p>- Communication</p><p>- Knowledge<br />&#160;&#160;- Existence of other players<br />&#160;&#160;- Actions of others<br />&#160;&#160;- Payoffs of others and outsiders</p><p>- Type of games<br />&#160;&#160;- Of conflicting interest<br />&#160;&#160;- Fully cooperative<br />&#160;&#160;- Competitive games: {Constant-sum, Zero-sum}</p><p>- Fictitious play, reinforce learning.</p><p>- The important thing is about the concept. (You have got to<br />&#160;&#160;understand the maths.)<br /></p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node text="20140907">
+<node text="President's choice                                        :Decision tree:[fn:1]">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>- Force students to share<br />&#160;&#160;- Fight it.<br />&#160;&#160;&#160;&#160;- President give include. (3,9)<br />&#160;&#160;&#160;&#160;- get kicked out. (-4,-5)<br />&#160;&#160;- Leave. (-5,1)<br />&#160;&#160;- Give include. (10,1)</p><p><br />- Not to share<br />&#160;&#160;- Fight in the next battle. (5,11)<br />&#160;&#160;- Be happy. (7,10)</p><p></p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node text="20140917">
+<node text="CR4, 13:30">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>- Strategy<br />&#160;&#160;- Pure strategy<br />&#160;&#160;- Mixed strategy<br />&#160;&#160;- Val = arg max_{pi_i} min_{pi_-i} r(pi_i,pi_-i)</p><p>- </p><p></p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node text="Footnotes">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><br />[fn:1] Extensive-form games.</p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
